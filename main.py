2305689aa6f66c86ffcd2ca9ef74d9dc461b330c
maruserushi <marcel.hubinger@smostec.com>
2026-02-14 15:55:25 +0100

    add and update asteroid/asteroidfield

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     clock = pygame.time.Clock()
     dt = 0
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    asteroidsfield = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        updatable.update(dt)
+        for updatables in updatable:
+            updatables.update(dt)
         for drawables in drawable:
             drawables.draw(screen)
         pygame.display.flip()
